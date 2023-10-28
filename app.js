0a867db5293a7f0c160fb6250525d41f36b289a5
omerfkgoze <farukucukgoze@gmail.com>
2023-10-28 21:21:25 +0200

    ticket #2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 
 import tourRouter from './routes/tourRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 export default app;
