94f5078afb854a54c104cd96d885815ffbd3cad1
omerfkgoze <farukucukgoze@gmail.com>
2023-10-29 01:25:52 +0200

    tickets #8 #12

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
 
+////
+//! bug: getting undefined without this line of code
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
+
+////
+
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import app from './app.js';
 import dotenv from 'dotenv';
-
 dotenv.config({ path: './config.env' });
 
-console.log(process.env);
+import app from './app.js';
 
-const port = process.env.PORT || 3000;
+// console.log(process.env);
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
