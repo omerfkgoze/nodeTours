9e07bb3df7ddaf35596eeef1c08f725cc3bdd91e
omerfkgoze <farukucukgoze@gmail.com>
2023-10-28 21:01:15 +0200

    ticket #1

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
+
 import tourRouter from './routes/tourRoutes.js';
 
 const app = express();
 
 // MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
