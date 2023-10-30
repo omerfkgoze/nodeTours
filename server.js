157662162ccaea4b248425745aaad3d61219c9e5
omerfkgoze <farukucukgoze@gmail.com>
2023-10-30 15:10:53 +0100

    ticket #20

--- server.js
+++ server.js
@@ -14,38 +14,6 @@ const DB = process.env.DATABASE.replace(
 // connect to DB
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
-// create a schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name!'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price!'],
-  },
-});
-
-// create a model
-const Tour = mongoose.model('Tour', tourSchema);
-
-// create a document
-const testTour = new Tour({
-  name: 'Hello DB',
-  price: 997,
-});
-
-// save the document to the database
-testTour
-  .save()
-  .then(doc => console.log(doc))
-  .catch(err => console.log('ERROR:', err));
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
