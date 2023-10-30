b25a1b2b25c170e1dbdb30a85299f51127fc405a
omerfkgoze <farukucukgoze@gmail.com>
2023-10-30 13:08:28 +0100

    ticket #15

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ dotenv.config({ path: './config.env' });
 
 // console.log(process.env);
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
