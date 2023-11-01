a54607a00d0d0eb571e9289fd151ffa452cc8951
omerfkgoze <farukucukgoze@gmail.com>
2023-11-01 01:21:55 +0100

    ticket #25

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import Tour from '../../models/tourModel.js';
+import Tour from '../../models/tourModel.js';
 
 dotenv.config({
   path: '../../.config.env',
@@ -17,3 +17,34 @@ mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync('./tours-simple.json', 'utf-8'));
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// CONTROL FLOW
+if (process.argv[2] === '--import') {
+  // node dev-data/data/import-dev-data.js --import
+  importData();
+} else if (process.argv[2] === '--delete') {
+  // node dev-data/data/import-dev-data.js --delete
+  deleteData();
+}
