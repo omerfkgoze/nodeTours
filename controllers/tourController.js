39a0f6117e450be5cd76d08e1a6ee1f807b171bd
omerfkgoze <farukucukgoze@gmail.com>
2023-11-04 00:10:45 +0100

    ticket #36

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -66,8 +66,8 @@ class APIFeatures {
 
   // PAGINATION
   paginate() {
-    const page = +this.query.page || 1;
-    const limit = +this.query.limit || 100;
+    const page = +this.queryString.page || 1;
+    const limit = +this.queryString.limit || 100;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
