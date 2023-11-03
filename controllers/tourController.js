177cb5c206df6357c97c006f1fcf80a55481189a
omerfkgoze <farukucukgoze@gmail.com>
2023-11-03 22:11:51 +0100

    ticket #30

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,6 +41,19 @@ const getAllTours = async (req, res) => {
       // query = query.select('-__v');
     }
 
+    // PAGINATION
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
