0812e3f85ca018df04745e79c397edb50ae7ee15
omerfkgoze <farukucukgoze@gmail.com>
2023-11-04 22:30:03 +0100

    ticket #39

--- models/tourModel.js
+++ models/tourModel.js
@@ -56,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
