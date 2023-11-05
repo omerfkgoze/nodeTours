cc8cb17febdc1d205f4f0963a9856f567f6db93c
omerfkgoze <farukucukgoze@gmail.com>
2023-11-05 19:15:28 +0100

    ticket #42

--- models/tourModel.js
+++ models/tourModel.js
@@ -9,6 +9,14 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name.'],
       unique: true,
       trim: true,
+      maxlength: [
+        40,
+        'A tour name must have less or equal than 40 characters.',
+      ],
+      minlength: [
+        10,
+        'A tour name must have more or equal than 10 characters.',
+      ],
     },
     slug: String,
     duration: {
