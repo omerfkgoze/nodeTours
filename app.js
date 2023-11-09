3f47891070ff672193b4dd81927556168c56ed60
omerfkgoze <farukucukgoze@gmail.com>
2023-11-09 22:48:11 +0100

    ticket #48

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
+
+import AppError from './utils/appError.js';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
 // MIDDLEWARES
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 
@@ -20,5 +22,10 @@ app.use(express.static('public'));
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// 404 HANDLER
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`));
+});
 
 export default app;
