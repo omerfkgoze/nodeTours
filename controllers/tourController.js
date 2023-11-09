c939d8acbb453c426a645304c347c937a0fb48e9
omerfkgoze <farukucukgoze@gmail.com>
2023-11-09 23:51:27 +0100

    ticket #55

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,30 +11,23 @@ const aliasTopTours = (req, res, next) => {
 };
 
 // GET ALL TOURS
-const getAllTours = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+const getAllTours = catchAsync(async (req, res) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    const tours = await features.query;
+  const tours = await features.query;
 
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: { tours },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
 
 // GET A TOUR
 const getTour = async (req, res) => {
