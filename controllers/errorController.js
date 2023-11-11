3b460772c02d86e4b217723731060082f02be9f1
omerfkgoze <farukucukgoze@gmail.com>
2023-11-11 22:58:01 +0100

    ticket #59

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,25 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 export default function (err, req, res, next) {
   // console.log(err.stack); // optional
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+  if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
 }
