1154341883d71074bdce1ad4bc79f1159cc48588
omerfkgoze <farukucukgoze@gmail.com>
2023-11-12 19:26:58 +0100

    ticket #65

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 import app from './app.js';
 
+// handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+
+  process.exit(1); // 0 success, 1 uncaught exception
+});
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
@@ -15,9 +23,10 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+// handle unhandled rejections
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
 
   // giving time to finish all the requests
   server.close(() => {
