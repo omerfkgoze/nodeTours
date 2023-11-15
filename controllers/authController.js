790b028b597663123cca2fb5c840dd5a7e12773e
omerfkgoze <farukucukgoze@gmail.com>
2023-11-15 17:17:46 +0100

    ticket #72

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,12 @@ import catchAsync from '../utils/catchAsync.js';
 
 // CREATE A USER
 const signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
