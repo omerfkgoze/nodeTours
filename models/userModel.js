1e35933a2317513bfe7de647cfccac1ab2c7050f
omerfkgoze <farukucukgoze@gmail.com>
2023-11-15 15:52:18 +0100

    ticket #71

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -39,6 +40,12 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
   // only run this function if password was actually modified
   if (!this.isModified('password')) return next();
+
+  // hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete passwordConfirm field
+  this.passwordConfirm = undefined;
 
   next();
 });
