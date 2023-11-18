deb16ee0d24bc324e408022c98b022ef3e2e56d9
omerfkgoze <farukucukgoze@gmail.com>
2023-11-18 23:53:16 +0100

    hide password in any output

--- models/userModel.js
+++ models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password.'],
     minlength: 8,
-    // select: false,
+    select: false,
   },
   passwordConfirm: {
     type: String,
