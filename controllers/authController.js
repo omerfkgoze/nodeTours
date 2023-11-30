e64097378315d41c938b813d6c6e5f2ba58de90c
omerfkgoze <farukucukgoze@gmail.com>
2023-11-30 19:56:58 +0100

    ticket #87

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import catchAsync from '../utils/catchAsync.js';
 import AppError from '../utils/appError.js';
+import sendEmail from '../utils/email.js';
 
 //* payload, secret, and options
 const signToken = function (id) {
@@ -124,6 +125,34 @@ const forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false }); // turn off all validators
 
   // 3) SEND IT TO USER'S EMAIL
+  const resetURL = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 minutes)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    // reset the passwordResetToken and passwordResetExpires
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false }); // turn off all validators
+
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500,
+    );
+  }
 });
 
 const resetPassword = catchAsync(async (req, res, next) => {});
