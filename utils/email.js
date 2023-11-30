77d87ce5a93f786e9f6dab5f68843e6c3a28bdd7
omerfkgoze <farukucukgoze@gmail.com>
2023-11-30 20:00:16 +0100

    edited mailOptions

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async (options) => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'NodeTours <process.env.EMAIL_FROM>',
+    from: `NodeTours <${process.env.EMAIL_FROM}>`,
     to: options.email,
     subject: options.subject,
     text: options.message,
